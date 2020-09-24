5ab9e33ce59722275f53668175fe9dca199b2fef
JerryLee <lifengxi1013@163.com>
2020-09-24 18:29:15 +0800

    06 vue基础语法介绍

--- 13_插件/vue-myPlugin.js
+++ 13_插件/vue-myPlugin.js
@@ -1,20 +1,31 @@
-(function (window) {
+(function () {
   const MyPlugin = {}
   MyPlugin.install = function (Vue, options) {
-    // 1. 添加全局方法或属性
+    // 1. 添加全局方法或 property
     Vue.myGlobalMethod = function () {
-      console.log('Vue函数对象的myGlobalMethod()')
+      // 逻辑...
+      console.log("Vue函数对象的方法");
     }
 
     // 2. 添加全局资源
-    Vue.directive('my-directive',function (el, binding) {
-      el.textContent = 'my-directive----'+binding.value
+    Vue.directive('my-directive', function (el,binding) {
+      el.textContent = binding.value.toUpperCase()
+    })
+
+    // 3. 注入组件选项
+    Vue.mixin({
+      created: function () {
+        // 逻辑...
+      }
+      
     })
 
     // 4. 添加实例方法
-    Vue.prototype.$myMethod = function () {
-      console.log('vm $myMethod()')
+    Vue.prototype.$myMethod = function (methodOptions) {
+      // 逻辑...
+      console.log("Vue的实例对象的方法$myMethod");
     }
   }
+  //向外暴漏
   window.MyPlugin = MyPlugin
-})(window)
+})()
