e9041d500065c457c889cadc91d03e0832e16438
JerryLee <lifengxi1013@163.com>
2020-09-25 23:51:18 +0800

    24 src-users备份

---
+++ VueApp/src-users/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <Search/>
+    <Main/>
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+import Search from './components/Search'
+import Main from './components/Main'
+export default {
+  name: 'App',
+  mounted () {
+  },
+  components: {
+    Search,
+    Main
+  }
+}
+</script>
+
+<style>
+
+</style>
