e0d39da050b3566d0fe894030e2a8c8caba0db5f
JerryLee <lifengxi1013@163.com>
2020-09-25 23:27:26 +0800

    22 ajax请求练习,注意PubSub组件应用的方式。

--- VueApp/src/App.vue
+++ VueApp/src/App.vue
@@ -1,44 +1,21 @@
 <template>
-  <div>
-    <div v-if="!repoUrl">loading</div>
-    <div v-else>most star repo is <a :href="repoUrl">{{repoName}}</a></div>
+  <div class="container">
+    <Search/>
+    <Main/>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+// import axios from 'axios'
+import Search from './components/Search'
+import Main from './components/Main'
 export default {
   name: 'App',
-  data () {
-    return {
-      repoUrl: '',
-      repoName: ''
-    }
-  },
   mounted () {
-    //  发ajax请求
-    const url = `https://api.github.com/search/repositories?q=vue&sort=stars`
-    // this.$http.get(url).then(
-    //   response => {
-    //     const result = response.data
-    //     const mostRepo = result.items[0]
-    //     this.repoUrl = mostRepo.html_url
-    //     this.repoName = mostRepo.name
-    //   },
-    //   response => {
-    //     alert('请求失败')
-    //   }
-    // )
-
-    //  使用axios发送请求
-    axios.get(url).then(response => {
-      const result = response.data
-      const mostRepo = result.items[0]
-      this.repoUrl = mostRepo.html_url
-      this.repoName = mostRepo.name
-    }).catch(error => {
-      alert(error)
-    })
+  },
+  components: {
+    Search,
+    Main
   }
 }
 </script>
